4bd34cf96b356d4b9c0de3d878f853d83895401f
elizzaveta <lizatimcenko5@gmail.com>
2020-04-24 16:43:58 +0300

    Added work with cmd arguments

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -2,7 +2,29 @@
 
 using namespace std;
 
-int main()
+void arguments(int argc, char* argv[], char& option, string& output, string* input);
+
+int main(int argc, char* argv[])
 {
 	cout << "Hello, world!" << endl;
+
+
+	if (argc >= 4) {
+		char option;
+		string output;
+		string* input = new string[argc - 3];
+		arguments(argc, argv, option, output, input);
+	
+		//test:
+		cout << "Your option is --" << option << endl;
+		cout << "Output file is " << output << endl;
+		cout << "Input files: ";
+		for (int i = 3; i < argc; i++) {
+			cout << input[i - 3] << " ";
+		}
+	}
+	else {
+		cout << "You need to run the program from cmd";
+	}
+
 }
